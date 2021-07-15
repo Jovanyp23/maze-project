97d6769fdb44400df3885a76adb1fef3722671e6
Jovany <jovanypena@csus.edu>
2021-07-15 15:56:23 -0700

    Funcs to create and display maze walls

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const width= 600;
 const height=600;
 const cells = 3;
 
+const unitLength= width/cells; 
+
+
 const engine = Engine.create();
 const {world} = engine; 
 const render = Render.create ({
@@ -108,13 +111,41 @@ const stepThroughCell = (row, column) => {
 
 stepThroughCell(startRow, startColumn);
 
-horizontals.forEach(row => {
-    row.forEach((open) => {
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
         if (open){
             return;
         }
 
-        const wall = Bodies.rectangle();
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength /2,
+            rowIndex * unitLength + unitLength,
+            unitLength,
+            10,
+            {
+                isStatic:true
+            }
+        );
+        World.add(world, wall);
+    });
+});
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength /2,
+            10,
+            unitLength,
+            {
+                isStatic:true
+            }
+        );
+        World.add(world,wall);
     });
 });
 
